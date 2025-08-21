d59d7288df6daa5518a237a0173b296fccb0f88f
= <=>
2025-08-21 20:32:56 +0300

    images and caches

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"871f65ac1bf129edb222c3293a636ff4b67534
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3889753848"
+    serviceWorkerVersion: "2556897692"
   }
 });
